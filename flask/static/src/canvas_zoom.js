b021953ffcfa611e2b0138ffdc56795ced6e01e3
Dj-Polyester <anonymous_571@outlook.com>
2022-08-15 03:16:55 +0300

    zoom complete (for now)

--- flask/static/src/canvas_zoom.js
+++ flask/static/src/canvas_zoom.js
@@ -14,17 +14,26 @@ function moveCanvasBy(oldpos, delta) {
     return newpos;
 }
 
+function scaleCanvas(pm) {
+    clearCanvas();
+    canvasMoveX = pm * canvas.width * ZOOM_CONSTANT;
+    canvasMoveY = pm * canvas.height * ZOOM_CONSTANT;
+    canvas.width -= canvasMoveX;
+    canvas.height -= canvasMoveY;
+    cellWidth -= pm * cellWidth * ZOOM_CONSTANT;
+    cellHeight -= pm * cellHeight * ZOOM_CONSTANT;
+    addGridOnCanvas();
+    deltaTmp = {
+        x: canvasMoveX / 2,
+        y: canvasMoveY / 2,
+    }
+    canvasPos = moveCanvasBy(canvasPos, deltaTmp);
+}
+
 document.body.addEventListener('wheel', function (event) {
 
     const pm = Math.sign(event.deltaY);
-    clearCanvas();
-
-    canvas.width -= pm * canvas.width * ZOOM_CONSTANT;
-    canvas.height -= pm * canvas.height * ZOOM_CONSTANT;
-    cellWidth -= pm * cellWidth * ZOOM_CONSTANT;
-    cellHeight -= pm * cellHeight * ZOOM_CONSTANT;
-
-    addGridOnCanvas();
+    scaleCanvas(pm);
 
     delta = {
         x: pm * (event.clientX - document.body.clientWidth / 2),
@@ -36,8 +45,6 @@ document.body.addEventListener('wheel', function (event) {
     delta.y /= deltaNormal;
     delta.x *= ZOOM_MOVE_MULTIPLER;
     delta.y *= ZOOM_MOVE_MULTIPLER;
-
     console.log(delta);
-
     canvasPos = moveCanvasBy(canvasPos, delta);
 }, true);
